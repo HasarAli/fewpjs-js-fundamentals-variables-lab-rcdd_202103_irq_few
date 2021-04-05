3713b056391548e0fdd9ce122d6cac4eed6bf3e8
Hasar Ali <hasar.ali@protonmail.com>
2021-04-05 10:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 
-const mostPRofitableNeighborhood = 'Chealsea';
+const mostProfitableNeighborhood = 'Chealsea';
 
 const companyCeo = 'Susan Smith';
